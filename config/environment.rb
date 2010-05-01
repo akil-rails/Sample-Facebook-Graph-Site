8e43083fe454f0e34d6eba355efd4dba354ae0c9
Sam Schenkman-Moore <samsm@samsm.com>
2010-04-30 23:06:48 -0400

    Sample facebook_keys file.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-FACEBOOK_CONF = YAML::load(IO.read("#{Rails.root}/config/facebook_keys.yml"))['development']
+if File.exist?("#{Rails.root}/config/facebook_keys.yml")
+  FACEBOOK_CONF = YAML::load(IO.read("#{Rails.root}/config/facebook_keys.yml"))['development']
+else
+  raise "Setup config/facebook_keys.yml"
+end
 
 # Initialize the rails application
 Fbgraph::Application.initialize!
